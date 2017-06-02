9f4e2f26e91ad31a4fc27e26fa2a16d1f537e1f2
David Tarditi <dtarditi@microsoft.com>
2017-06-02 00:12:41 -0700

    Revert "Removes Checked Headers, Instead Includes Headers from Spec Repository (#44)"
    This reverts commit 7e961cdde1c178de3db111209d7ac1eaf14ef6f5.

---
+++ include/inttypes_checked.h
@@ -0,0 +1,31 @@
+//---------------------------------------------------------------------//
+// Bounds-safe interfaces for functions in inttypes.h that             //
+// take pointer arguments.                                             //
+//                                                                     //
+// These are listed in the same order that they occur in the C11       //
+// specification.                                                      //
+//                                                                     //
+// TODO: revise string types after support for pointers to             //
+// null-terminated arrays is added to C.                               //
+/////////////////////////////////////////////////////////////////////////
+
+#include <stddef.h> // define wchar_t for wcstoimax and wcstoumax
+#include <inttypes.h>
+
+#pragma BOUNDS_CHECKED ON
+
+intmax_t strtoimax(const char * restrict nptr,
+                   char ** restrict endptr : itype(restrict _Ptr<char *>),
+                   int base);
+uintmax_t strtoumax(const char * restrict nptr,
+                    char ** restrict endptr : itype(restrict _Ptr<char *>),
+                    int base);
+
+intmax_t wcstoimax(const wchar_t * restrict nptr,
+                   wchar_t ** restrict endptr : itype(restrict _Ptr<wchar_t *>),
+                   int base);
+uintmax_t wcstoumax(const wchar_t * restrict nptr,
+                    wchar_t ** restrict endptr : itype(restrict _Ptr<wchar_t *>),
+                    int base);
+
+#pragma BOUNDS_CHECKED OFF
