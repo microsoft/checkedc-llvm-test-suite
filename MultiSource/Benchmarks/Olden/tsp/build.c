3ffd101de5ea5a81a2c9cd82c612f6f4efecdadc
prabhu <t-prr@microsoft.com>
2018-08-20 16:50:10 -0700

    Polymorphic bounds safe interface function signature for stdlib functions. Replacing macro definition of calloc to accept type application parameter.

--- MultiSource/Benchmarks/Olden/tsp/build.c
+++ MultiSource/Benchmarks/Olden/tsp/build.c
@@ -82,9 +82,8 @@ Tree build_tree(int n,int dir,int lo,int num_proc,double min_x,
 #endif
 
   if (n==0) return NULL;
-  _Unchecked {
-  t = (Tree) ALLOC(lo,sizeof(*t));
-  }
+
+  t = (Tree) ALLOC(struct tree, lo, sizeof(*t));
 
   if (dir) {
     dir = !dir;
