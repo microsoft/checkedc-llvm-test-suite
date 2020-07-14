056ef69273731225ac28258336f08a943b6346f1
Mandeep Singh Grang <magrang@microsoft.com>
2020-07-14 12:53:31 -0700

    Fix

--- MultiSource/Benchmarks/Ptrdist/yacr2/vcg.c
+++ MultiSource/Benchmarks/Ptrdist/yacr2/vcg.c
@@ -229,7 +229,7 @@ DFSBelowVCG(_Array_ptr<nodeVCGType> VCG : count(channelNets + 1),
 void
 SCCofVCG(_Array_ptr<nodeVCGType> VCG : count(channelNets + 1),
 	     _Array_ptr<ulong> SCC : count(channelNets + 1),
-		 _Array_ptr<ulong> paramSCC : count(totalSCC + 1))
+		 _Array_ptr<ulong> tmpPerSCC : count(totalSCC + 1))
 {
     ulong      	net;
     ulong      	scc;
@@ -242,7 +242,7 @@ SCCofVCG(_Array_ptr<nodeVCGType> VCG : count(channelNets + 1),
     ;
 
     ulong originalTotalSCC = totalSCC;
-    _Array_ptr<ulong> perSCC : count(originalTotalSCC + 1) = paramSCC;
+    _Array_ptr<ulong> perSCC : count(originalTotalSCC + 1) = tmpPerSCC;
 
     /*
      * DFS of above edges.
